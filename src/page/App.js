02bb5a6f2eeee62080c332305f1e4e11174c7169
MSAC98 <44638120+MSAC98@users.noreply.github.com>
2022-03-26 21:56:08 +0100

    opdatering af app.js

--- src/page/App.js
+++ src/page/App.js
@@ -1,8 +1,13 @@
 import logo from '../assets/logo.svg';
 import './App.css';
+import RoomPage from '../component/Room/RoomPage'
 
 function App() {
   return (
+    <div>
+      <RoomPage></RoomPage>
+    </div>
+    /*
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +24,7 @@ function App() {
         </a>
       </header>
     </div>
+    */
   );
 }
 
