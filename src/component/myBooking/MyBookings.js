625f09e77ef2b95b2d08794c1438bce4a3786a28
s185736 <65380030+s185736@users.noreply.github.com>
2022-04-08 09:22:23 +0200

    Small update.

--- src/component/myBooking/MyBookings.js
+++ src/component/myBooking/MyBookings.js
@@ -2,18 +2,22 @@ import React from 'react';
 
 /**
  * Author: Azmi Uslu (s185736)
+ * Status: still alot to do..
  **/
 
 export default function MyBookings() {
 
     return (
         <div>
-            <div className="blockBehind BasketDetailsRoom">
+            <div className="blockBehind BookingsDetailsRoom">
                 <h2>My Orders</h2>
-                <div className="RoomDetailColumn">
-                Order 1
-                Order 1
-                Order 1
+                <div className="OrderDetailColumn">
+                    Order 1: Budget Room, booked 01-04-2022.
+                    <div></div>
+                    Order 2: Business Room, booked 01-04-2020.
+                    <div></div>
+                    Order 3: Luxury Room, booked 01-04-2018.
+                    <div></div>
                 </div>
             </div>
         </div>
