6af7ed12332906ec57b80a992d0df6446b93c75a
Repeller <29373163+Repeller@users.noreply.github.com>
2022-04-18 11:54:58 +0200

    started working on the dbHelper file (for firebase CRUD functions)

---
+++ src/component/dbHelper.js
@@ -0,0 +1,160 @@
+import { getDatebase, ref, set } from "firebase/database";
+
+// THIS FILE IS MADE BY:
+// Alexander Ziegler, S181100
+
+// this is not a component
+
+// booking CRUD
+function dbBookingRead(){
+
+}
+
+function dbBookingUpdate(){
+     
+}
+
+function dbBookingDelete(){
+     
+}
+
+function dbBookingCreate(){
+     
+}
+
+
+// booking_occupants CRUD
+function dbBookingOccupRead(){
+
+}
+
+function dbBookingOccupUpdate(){
+     
+}
+
+function dbBookingOccupDelete(){
+     
+}
+
+function dbBookingOccupCreate(){
+     
+}
+
+// floor CRUD
+function dbFloorRead(){
+
+}
+
+function dbFloorUpdate(){
+     
+}
+
+function dbFloorDelete(){
+     
+}
+
+function dbFloorCreate(){
+     
+}
+
+// room CRUD
+function dbRoomRead(){
+
+}
+
+function dbRoomUpdate(){
+     
+}
+
+function dbRoomDelete(){
+     
+}
+
+function dbRoomCreate(){
+     
+}
+
+// user CRUD
+function dbUserRead(){
+
+}
+
+function dbUserUpdate(){
+     
+}
+
+function dbUserDelete(){
+     
+}
+
+function dbUserCreate(){
+     
+}
+
+// room_type CRUD
+function dbRoomTypeRead(){
+
+}
+
+function dbRoomTypeUpdate(){
+     
+}
+
+function dbRoomTypeDelete(){
+     
+}
+
+function dbRoomTypeCreate(){
+     
+}
+
+// master_booking CRUD
+function dbMasterBookRead(){
+
+}
+
+function dbMasterBookUpdate(){
+     
+}
+
+function dbMasterBookDelete(){
+     
+}
+
+function dbMasterBookCreate(){
+     
+}
+
+// water_view CRUD
+function dbWaterViewRead(){
+
+}
+
+function dbWaterViewUpdate(){
+     
+}
+
+function dbWaterViewDelete(){
+     
+}
+
+function dbWaterViewCreate(){
+     
+}
+
+// extra_billing CRUD
+function dbExtraBillRead(){
+
+}
+
+function dbExtraBillUpdate(){
+     
+}
+
+function dbExtraBillDelete(){
+     
+}
+
+function dbExtraBillCreate(){
+     
+}
