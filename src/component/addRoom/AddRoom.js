c49afc47abb7308b8e49c50d55397383b576a0f8
Repeller <29373163+Repeller@users.noreply.github.com>
2022-05-08 21:58:59 +0200

    now the booking get added to firebase too

--- src/component/addRoom/AddRoom.js
+++ src/component/addRoom/AddRoom.js
@@ -3,6 +3,7 @@ import {useHistory} from 'react-router-dom';
 import {AppContext} from "../../AppContext/AppContext";
 import {Room} from "../../models/Room";
 import {RoomForm} from "../roomForm/RoomForm";
+import {dbCreateOne} from "../dbHelper";
 
 /**
  * Author: Azmi Uslu (s185736)
@@ -21,6 +22,26 @@ export const AddRoom = () => {
 			const roomToBeAdded = new Room(fullName, emailAddress, phoneNumber, roomTypeSelection, adultNumber, checkinDate, checkoutDate, childNumber);
 			addObject('room', roomToBeAdded);
 			history.push(`/booking/goto/${roomToBeAdded.id}`);
+
+			var d = Date(Date.now());
+
+			dbCreateOne("rooms", 
+			{
+				_fullName: fullName,
+				_emailAddress: emailAddress, 
+				_phoneNumber: phoneNumber, 
+				_roomTypeSelection: roomTypeSelection, 
+				_adultNumber: adultNumber, 
+				_checkinDate: checkinDate,
+				_checkoutDate: checkoutDate, 
+				_childNumber: childNumber,
+				_createOrder: d.toString(),
+				_id: "fix this later",
+				_bookings: "",
+				_updateOrder: ""
+			})
+			.then(() => {console.log("room was added to firestore")})
+			.catch((err) => {console.log("room added error:", err)});
 		}}/>
   )
 };
